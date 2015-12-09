36e7b99c11d456b109d635bac33a1929b5f4f240
brijesh <brijesh.kumar@daffodilsw.com>
2015-12-09 19:12:37 +0530

    add color in headers

--- public/app.js
+++ public/app.js
@@ -2,6 +2,8 @@ var app = angular.module('myApp',[]);
 
 app.controller('myController',function($scope, 	$http){
 	
+	$scope.userLogin = false;
+
 	$http.get('/users').then(function(response){
 		$scope.users = response.data;
 	});
